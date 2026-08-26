9afdeb96181495f2663f7fb0d4bbf7cc019922de
Tao <tao.spa2003@gmail.com>
2026-08-26 18:05:07 +0200

    [PROGRESS] pairplot: histogram for self-compared courses

--- src/pair_plot.py
+++ src/pair_plot.py
@@ -50,23 +50,32 @@ def pair_plot(filename: str | Path) -> None:
         for comp_class in classes:
             ax = axes.flat[index_graph]
             for house in houses:
-                x_raw = grades[classes[classe]][house]
-                y_raw = grades[classes[comp_class]][house]
-                
-                pairs = []
-                for x, y in zip(x_raw, y_raw):
-                    if x is not None and y is not None:
-                        pairs.append((x, y))
-                if not pairs:
-                    continue
-                x, y = zip(*pairs)  # it splits a list of (x, y) tuples back into two separate sequences.
-                                    # ex: ((1, 2), (3, 4), (5, 6)) -> (1, 3, 5), (2, 4, 6)
-                ax.scatter(x, y, color=houses[house], label=house, alpha = 0.25)
+                if classe == comp_class:
+                    data = [g for g in grades[classes[classe]][house] if g is not None]
+                    ax.hist(
+                            data,
+                            bins=20,
+                            alpha=0.5,
+                            color=houses[house]
+                            )
+                else:
+                    x_raw = grades[classes[classe]][house]
+                    y_raw = grades[classes[comp_class]][house]
+                    
+                    pairs = []
+                    for x, y in zip(x_raw, y_raw):
+                        if x is not None and y is not None:
+                            pairs.append((x, y))
+                    if not pairs:
+                        continue
+                    x, y = zip(*pairs)  # it splits a list of (x, y) tuples back into two separate sequences.
+                                        # ex: ((1, 2), (3, 4), (5, 6)) -> (1, 3, 5), (2, 4, 6)
+                    ax.scatter(x, y, color=houses[house], label=house, alpha = 0.25)
 
             if index_graph == 13 * 13 - 1:
                 ax.legend()
-            if (classes[classe] == 0):
-                ax.set_title(comp_class[:12] + '.' if len(comp_class) > 8 else comp_class)
+            if (classes[classe] == 12):
+                ax.set_xlabel(comp_class[:12] + '.' if len(comp_class) > 8 else comp_class)
             if (classes[comp_class] == 0):
                 ax.set_ylabel(classe[:10] + '.' if len(classe) > 8 else classe)
             index_graph += 1
